e1748120131080fd5cb9e8d0399920c22025cdb5
Ankita-Kumari-bhardwaj <107136820+Ankita-Kumari-bhardwaj@users.noreply.github.com>
2025-11-13 07:59:53 +0530

    program to calculate sum of numbers from 1 to 50
    This program calculates the sum of integers from 1 to 50 using a for loop.

---
+++ Assignment_2_Task 2.py
@@ -0,0 +1,19 @@
+"""
+------------------------------------------------------------
+Title: Assignment 2 Task 2 - Sum of Numbers
+Author: Ankita Bhardwaj
+Date: November 2025
+Description:
+    This program calculates the sum of integers from 1 to 50 
+    using a for loop.
+------------------------------------------------------------
+"""
+
+s = 0
+
+# for loop to iterate over numbers from 1 to 50
+for i in range(1, 51):
+    s = s + i
+
+# Displaying the final sum
+print("The sum of numbers from 1 to 50 is:", s)
