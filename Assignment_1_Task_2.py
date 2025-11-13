6088e4df8eb7e9b28d65886cfa3a11d9a5e636a0
Ankita-Kumari-bhardwaj <107136820+Ankita-Kumari-bhardwaj@users.noreply.github.com>
2025-11-13 06:53:52 +0530

    Add greeting program that takes user input
    This program prompts the user for their first and last name and displays a personalized greeting message.

---
+++ Assignment_1_Task_2.py
@@ -0,0 +1,17 @@
+"""
+------------------------------------------------------------
+Title: Task 2 - Greeting Program
+Author: Ankita Bhardwaj
+Date: November 2025
+Description:
+    This program takes two strings (first name and last name)
+    as input from the user and prints a personalized greeting.
+------------------------------------------------------------
+"""
+
+# Taking user input
+firstName = input("Enter the first name: ")
+lastName = input("Enter the last name: ")
+
+# Displaying greeting message
+print("Hello", firstName + " " + lastName + "!", "Welcome to the Python program.")
