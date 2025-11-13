f72cd082c76fb5f284fa509f107870bdaa0e5198
Ankita-Kumari-bhardwaj <107136820+Ankita-Kumari-bhardwaj@users.noreply.github.com>
2025-11-13 07:52:47 +0530

    Add odd-even checker program
    This program checks if a user-provided number is even or odd.

---
+++ Assignment 2_Task_1.py
@@ -0,0 +1,19 @@
+"""
+------------------------------------------------------------
+Title: Assignment 2 Task 1 - Odd Even Program
+Author: Ankita Bhardwaj
+Date: November 2025
+Description:
+    This program takes a number from the user and Checks whether the number is even or odd using an if-else 
+------------------------------------------------------------
+"""
+
+# Taking user input
+n=int(input("Enter a number: "))
+
+# Displaying result
+
+if n%2==0:
+    print(n,"is an even number")
+else:
+    print(n, "is an Odd number")
