b5d16a047a650eba5c7527898ca2213811576199
Ankita-Kumari-bhardwaj <107136820+Ankita-Kumari-bhardwaj@users.noreply.github.com>
2025-11-13 06:47:45 +0530

    Update

--- Assignment_1 _task1.py
+++ Assignment_1 _task1.py
@@ -21,6 +21,7 @@ num2 = float(input("Enter the second number: "))
 addition = num1 + num2
 subtraction = num1 - num2
 multiplication = num1 * num2
+division = num1 / num2
 
 # Displaying the results
 print("\n")
