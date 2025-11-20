60e19850c1c7b66a885086593e00100c6d6b715c
Ankita-Kumari-bhardwaj <107136820+Ankita-Kumari-bhardwaj@users.noreply.github.com>
2025-11-20 08:44:43 +0530

    Add Task_2.py to extract and reverse list elements

---
+++ Assignment5/Task_2.py
@@ -0,0 +1,12 @@
+#creating a list
+num= [1,2,3,4,5,6,7,8,9,10]
+
+#printing original list
+print(f"Original list: {num}")
+
+#extracting and printing first five elements
+print(f"Extracted first five elements: {num[0:5]}")
+
+# extracting and printing reversed list
+print(f"Reversed extracted elements: {(num[4::-1])}")
+
